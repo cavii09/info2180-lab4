a41a75c7f1cd891ff46cf60209f3a96d3c3d951e
Cavel Peck <cavelpeck@gmail.com>
2021-10-30 19:26:20 -0500

    Updated index.html and app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 window.onload = function(){
-    
-}
+    var searchBtn = document.getElementById("btn")
+    var httpRequest;
+
+    searchBtn.addEventListener('click', function(element){
+        element.preventDefault();
+
+        httpRequest = new XMLHttpRequest();
+
+        var url = "http://localhost/info2180-lab4/superheroes.php";
+        httpRequest.onreadystatechange =function searchLog()
+        {
+            if(httpRequest.readyState == XMLHttpRequest.DONE){
+                if(httpRequest.status == 200){
+                    var response = httpRequest.responseText;
+                }else{
+                    alert('There was a problem with the request.')
+                }
+            }
+        }
+        httpRequest.open('GET', url);
+        httpRequest.send();
+        
+        });
+        
+
+    }        
