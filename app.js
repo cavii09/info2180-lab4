67c22c310d2b9629a94822a5d31885d582b55632
Cavel Peck <cavelpeck@gmail.com>
2021-10-31 11:23:08 -0500

    Edited with clicked button and search of superheoes index.html and app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     var searchBtn = document.getElementById("btn")
     var httpRequest;
+    
 
     searchBtn.addEventListener('click', function(element){
         element.preventDefault();
@@ -8,20 +9,21 @@ window.onload = function(){
         httpRequest = new XMLHttpRequest();
 
         var url = "http://localhost/info2180-lab4/superheroes.php";
-        httpRequest.onreadystatechange =function searchLog()
-        {
-            if(httpRequest.readyState == XMLHttpRequest.DONE){
-                if(httpRequest.status == 200){
-                    var response = httpRequest.responseText;
-                }else{
-                    alert('There was a problem with the request.')
-                }
-            }
-        }
+        httpRequest.onreadystatechange = searchLog;
         httpRequest.open('GET', url);
         httpRequest.send();
         
         });
-        
+        function searchLog()
+        {
+            if(httpRequest.readyState == XMLHttpRequest.DONE){
+                if(httpRequest.status == 200){
+                    var response = httpRequest.responseText;
+                    alert(response);
+                }else{
+                    alert('There was a problem with the request.');
+                }
+            }
+        }
 
     }        
